be85ea787abdf9f539834985266b3cbccee6c2f3
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2026-03-01 16:34:39 +0530

    1 March 2026 : Update update_streak script

--- scripts/update_streak.py
+++ scripts/update_streak.py
@@ -1,25 +1,30 @@
 import os
 from datetime import datetime
 
-LOG_DIR = "logs/2026"
+LOG_DIR = "logs\\2026"
 
 today = datetime.now().strftime("%m-%d")
+print(today)
 log_file = os.path.join(LOG_DIR, f"{today}.md")
+print(log_file)
 
 streak = 0
 
 if os.path.exists(log_file):
-    streak = 1  # basic example
+    streak = streak + 1  # basic example
 
 readme_path = "README.md"
 
 with open(readme_path, "r") as f:
     content = f.read()
+    
 
 new_content = content.replace(
     "0 days",
     f"{streak} days"
 )
+
+print(new_content)
 
 with open(readme_path, "w") as f:
     f.write(new_content)
